def1f46ad572e6557cdbc443c4d4ff45f64b122e
Clifton Regis <github email address>
2016-08-03 17:24:12 -0400

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears 
+VALUES (1, "Mr. Chocolate", 20, "M", "Brown", "Sad", 1);
+INSERT INTO bears 
+VALUES (2, "Rowdy", 10, "M", "Black", "goofy", 1);
+INSERT INTO bears 
+VALUES (3, "Tabitha", 6, "F", "White", "Mad", 1);
+INSERT INTO bears 
+VALUES (4, "Sergeant Brown", 18, "M", "Brown", "Tired", 1);
+INSERT INTO bears 
+VALUES (5, "Melissa", 13, "F", "Brown", "Lazy", 1);
+INSERT INTO bears 
+VALUES (6, "Ginch", 2, "M", "White", "Sleepy", 1);
+INSERT INTO bears 
+VALUES (7, "Wendy", 6, "F", "Brown", "Droopy", 1);
+INSERT INTO bears 
+VALUES (8, NULL, 16, "M", "Brown", "Sad", 1)
